45043b22b938272e922289930fdac0e24ff7a680
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2025-12-24 21:33:58 +0300

    update 24.12.2025 Ver-0.1.2

--- App/agent/nodes/retrieval_node.py
+++ App/agent/nodes/retrieval_node.py
@@ -49,16 +49,11 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 
-import streamlit as st
-def get_secret(name: str):
-    return st.secrets.get(name) or os.getenv(name)
-
-
 # --- API Anahtarı ---
-OPENAI_API_KEY: str = get_secret("OPENAI_API_KEY")
+OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY","")
 
 # --- MongoDB Ayarları ---
-MONGO_URI: str = get_secret("MONGO_URI")
+MONGO_URI: str = os.getenv("MONGO_URI","")
 DB_NAME: str = os.getenv("DB_NAME", "YasaaVisionDB")
 COLLECTION_NAME: str = os.getenv("COLLECTION_NAME", "palmistry_knowledge")
 INDEX_NAME: str = os.getenv("INDEX_NAME", "vector_index")
